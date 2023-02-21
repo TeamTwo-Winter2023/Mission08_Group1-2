53314ff07802c9ddd806484c53a948d64a4dbe9a
Trevor Heath <trevormheath@gmail.com>
2023-02-21 10:08:56 -0700

    Add project files.

---
+++ Mission08_Group1-2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_Group1_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
