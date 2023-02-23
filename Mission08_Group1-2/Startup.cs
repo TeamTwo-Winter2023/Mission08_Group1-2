bfb171cdb5072c7f9e9a22ced1dfc386db39094e
matkinson5 <atkinsonmaryanne5@gmail.com>
2023-02-23 16:19:23 -0700

    Finishing Models:D

--- Mission08_Group1-2/Startup.cs
+++ Mission08_Group1-2/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission08_Group1_2.Models;
 
 namespace Mission08_Group1_2
 {
@@ -24,6 +26,11 @@ namespace Mission08_Group1_2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<TaskAdditionsContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:dbConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
